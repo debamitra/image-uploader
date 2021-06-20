c4a0cf7ad8e569eccdb48cc5d7d094f9406704d0
debamitra <debamitra.mukherjee@gmail.com>
2021-06-20 11:38:07 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -38,16 +38,13 @@ app.post('/upload', (req, res) => {
     });
 });
 
-app.get('/', (req, res) => {
-    res.send("Hey there");
-})
 
 
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
+
+const buildPath = path.join(__dirname, '..', 'build');
+
+app.use(express.static(buildPath));
 
 
 const port = process.env.PORT || 5000;
